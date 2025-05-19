d5d939121ef69718d3a01557e1bc457cabe99834
Maksim <143986439+AnkumaGithub@users.noreply.github.com>
2025-05-19 23:00:44 +0300

    BubbleSortStrategy

--- main.cpp
+++ main.cpp
@@ -9,3 +9,19 @@ public:
     virtual void sort(std::vector<int>& data) = 0;
 };
 
+class BubbleSortStrategy : public SortingStrategy {
+public:
+    void sort(std::vector<int>& data) override {
+        bool swapped;
+        for (size_t i = 0; i < data.size(); ++i) {
+            swapped = false;
+            for (size_t j = 0; j < data.size() - i - 1; ++j) {
+                if (data[j] > data[j + 1]) {
+                    std::swap(data[j], data[j + 1]);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+};
